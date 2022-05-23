244837871c734bd64494c35eb0c382b257e413f9
Mardonio Costa <mardonio.costa@tecnisys.com.br>
2022-05-23 15:07:27 -0300

    Adicionando template ao projeto

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,7 +20,7 @@ $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
-$routes->set404Override();
+$routes->set404Override(); // CRIAR PÁGINA DE ERRO CUSTOMIZADA
 $routes->setAutoRoute(true);
 
 /*
