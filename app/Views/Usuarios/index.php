ec98b5eaead01f2e41cfa09abd5ba55948b353b3
Mardonio Costa <mardonio.costa@tecnisys.com.br>
2022-05-25 10:30:44 -0300

    Instalando plugin dataTable

--- app/Views/Usuarios/index.php
+++ app/Views/Usuarios/index.php
@@ -6,63 +6,25 @@
 
 <?php echo $this->section('estilos'); ?>
 
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.12.1/r-2.3.0/datatables.min.css"/>
+
 <?php echo $this->endSection(); ?>
 
 <?php echo $this->section('conteudo'); ?>
 <div class="row">
   <div class="col-lg-12">
     <div class="ibox float-e-margins">
-      <div class="ibox-title">
-        <h5>Striped Table </h5>
-        <div class="ibox-tools">
-          <a class="collapse-link">
-            <i class="fa fa-chevron-up"></i>
-          </a>
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-            <i class="fa fa-wrench"></i>
-          </a>
-          <ul class="dropdown-menu dropdown-user">
-            <li><a href="#">Config option 1</a>
-            </li>
-            <li><a href="#">Config option 2</a>
-            </li>
-          </ul>
-          <a class="close-link">
-            <i class="fa fa-times"></i>
-          </a>
-        </div>
-      </div>
       <div class="ibox-content">
   
-        <table class="table table-striped">
+        <table id="ajaxTable" class="table table-striped" style="width: 100%;">
           <thead>
             <tr>
-              <th>#</th>
-              <th>Data</th>
-              <th>User</th>
-              <th>Value</th>
+              <th>Imagem</th>
+              <th>Nome</th>
+              <th>E-mail</th>
+              <th>Situação</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td><span class="line">5,3,2,-1,-3,-2,2,3,5,2</span></td>
-              <td>Samantha</td>
-              <td class="text-navy"> <i class="fa fa-level-up"></i> 40% </td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td><span class="line">5,3,9,6,5,9,7,3,5,2</span></td>
-              <td>Jacob</td>
-              <td class="text-warning"> <i class="fa fa-level-down"></i> -20% </td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td><span class="line">1,6,3,9,5,9,5,3,9,6,4</span></td>
-              <td>Damien</td>
-              <td class="text-navy"> <i class="fa fa-level-up"></i> 26% </td>
-            </tr>
-          </tbody>
         </table>
       </div>
     </div>
@@ -73,4 +35,7 @@
 <?php echo $this->endSection(); ?>
 
 <?php echo $this->section('scripts'); ?>
+
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.12.1/r-2.3.0/datatables.min.js"></script>
+
 <?php echo $this->endSection(); ?>
